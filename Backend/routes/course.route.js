4d351ce276c189d04a88b02963794d52cd2af040
sanjeetKumarThakur <157505074+sanjeetKumarThakur@users.noreply.github.com>
2025-05-06 18:26:15 +0545

    Update course.route.js

--- Backend/routes/course.route.js
+++ Backend/routes/course.route.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import { createCourse,updateCourse,deleteCourse,getCourse,courseDetails } from '../controllers/course.controllers.js';
+ import express from 'express';
+import { createCourse,updateCourse,deleteCourse,getCourse,courseDetails,buyCourses  } from '../controllers/course.controllers.js';
+import userMiddleware from '../middleware/user.mid.js';
+import adminMiddleware from '../middleware/admin.mid.js';
 
 const router = express.Router();
-router.post('/create', createCourse);
-router.put("/update/:courseId", updateCourse);
-router.delete('/delete/:courseId', deleteCourse);
+router.post('/create',adminMiddleware, createCourse);
+router.put("/update/:courseId", adminMiddleware,updateCourse);
+router.delete('/delete/:courseId',adminMiddleware, deleteCourse);
+
 router.get('/courses', getCourse);
-router.get("/:coureseId",courseDetails)
+router.get("/:coureseId",courseDetails);
+
+router.get("/buy/:courseId",userMiddleware,buyCourses)
+
+
+
 
 export default router;
