c10e26809bb34165fbe4f03e56519300186066b1
sanjeetKumarThakur <157505074+sanjeetKumarThakur@users.noreply.github.com>
2025-05-06 18:28:06 +0545

    Update user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,10 +1,12 @@
-import express from "express";
-import { login,logout,signup } from "../controllers/user.controller.js";
+ import express from "express";
+import { login,logout,signup ,purchases} from "../controllers/user.controller.js";
+import userMiddleware from "../middleware/user.mid.js";
 
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login",login);
 router.post("/logout", logout);
+router.get("/purchases",userMiddleware,purchases);
 
 
 
